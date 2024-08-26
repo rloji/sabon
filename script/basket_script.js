74aa12f17d92771bda82748f4cec632cc854792e
Jeong <168152974+JJong-Ara@users.noreply.github.com>
2024-08-26 15:40:12 +0900

    Add files via upload

--- script/basket_script.js
+++ script/basket_script.js
@@ -1,16 +1,4 @@
 $(function(){
-    // 스크롤 시 헤더 올라가다 메뉴에서 고정
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('#wrap header .logo').stop().hide(); // 로고 영역을 즉시 숨김
-            $('#wrap header').css('height', '0px'); // 헤더 높이 줄이기
-            $('#wrap header nav').css('top', '0px'); // 메뉴를 최상단으로 이동
-        } else {
-            $('#wrap header .logo').stop().show(); // 로고 영역을 즉시 표시
-            $('#wrap header').css('height', '298px'); // 헤더 높이를 원래대로 복구
-            $('#wrap header nav').css('top', 'auto'); // 메뉴 위치 원래대로 복구
-        }
-    });
 
     $(".menu-item").mouseenter(function() {
       // 모든 서브메뉴를 닫음
